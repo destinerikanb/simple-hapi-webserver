5759b86ff208a075d95b04f0e48b342fcfc13029
destinerikanb <destinerikanb@gmail.com>
2022-06-28 14:30:34 +0700

    [FEATURE] create http server using 3rd party module @hapi/hapi

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello. welcome to Hapi")
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+        port : 5000,
+        host : 'localhost',
+    })
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+}
+
+init();
